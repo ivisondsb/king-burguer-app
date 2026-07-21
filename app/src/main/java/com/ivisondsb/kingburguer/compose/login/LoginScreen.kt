c26f61258d0da8e73a62a52ca0166f8398e0e561
Ivison <ivison.dev@gmail.com>
2026-07-21 02:16:32 -0300

    feat: add password visibility toggle to KingTextField

--- app/src/main/java/com/ivisondsb/kingburguer/compose/login/LoginScreen.kt
+++ app/src/main/java/com/ivisondsb/kingburguer/compose/login/LoginScreen.kt
@@ -9,16 +9,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ivisondsb.kingburguer.R
@@ -27,6 +38,7 @@ import com.ivisondsb.kingburguer.ui.theme.KingBurguerTheme
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
     val scrollState = rememberScrollState()
+    var passwordHidden by remember { mutableStateOf(true) }
 
     Column(
         modifier
@@ -47,12 +59,28 @@ fun LoginScreen(modifier: Modifier = Modifier) {
         }
 
         KingTextField(
-            value = "",
+            value = "todo",
             label = R.string.password,
             placeholder = R.string.hint_password,
             keyboardType = KeyboardType.Password,
             imeAction = ImeAction.Done,
-            modifier = Modifier.padding(horizontal = 20.dp)
+            obfuscate = passwordHidden,
+            modifier = Modifier.padding(horizontal = 20.dp),
+            trailingIcon = {
+                IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                    val icon = if (passwordHidden) {
+                        Icons.Filled.VisibilityOff
+                    } else {
+                        Icons.Filled.Visibility
+                    }
+                    val description = if (passwordHidden) {
+                        stringResource(R.string.show_password)
+                    } else {
+                        stringResource(R.string.hide_password)
+                    }
+                    Icon(icon, contentDescription = description)
+                }
+            }
         ) {
 
         }
@@ -74,6 +102,8 @@ fun KingTextField(
     @StringRes placeholder: Int,
     keyboardType: KeyboardType = KeyboardType.Text,
     imeAction: ImeAction = ImeAction.Next,
+    obfuscate: Boolean = false,
+    trailingIcon: @Composable (() -> Unit)? = null,
     onValueChange: (String) -> Unit
 ) {
     OutlinedTextField(
@@ -90,7 +120,12 @@ fun KingTextField(
         keyboardOptions = KeyboardOptions(
             keyboardType = keyboardType,
             imeAction = imeAction
-        )
+        ),
+        trailingIcon = trailingIcon,
+        visualTransformation = if (obfuscate)
+            PasswordVisualTransformation()
+        else
+            VisualTransformation.None
     )
 }
 
